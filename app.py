c1430bfae88ac478630cd17eab4a3f64df4d6319
JakubKocerha <jakubbubble@gmail.com>
2021-12-29 18:58:20 +0000

    Add Register functionality

--- app.py
+++ app.py
@@ -18,19 +18,47 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
+# wires base.html up with home.html
 @app.route("/home")
 def home():
     return render_template('home.html')
 
-
+# gets data from MongoDB; code from CI tutorials
 @app.route("/get_posts")
 def get_posts():
     posts = mongo.db.posts.find()
     return render_template("posts.html", posts=posts)
 
-
-@app.route("/register")
+# adds register functionality; code from CI turorials 
+# excluding password confirmation validation
+@app.route("/register", methods=["GET", "POST"])
 def register():
+    password = request.form.get("password")
+    password_confirmation = request.form.get("password_confirmation")
+    # checks for matching passwords
+    if password != password_confirmation:
+        flash("Oops! Your passwords don't match. Try again.")
+        return redirect(url_for("register"))
+
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("This username is used! Please, choose a different one.")
+            return redirect(url_for("register"))
+
+        login = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get(
+                "password_confirmation"))
+        }
+        mongo.db.users.insert_one(login)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
     return render_template("register.html")
 
 
