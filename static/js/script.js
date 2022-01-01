83544dd13f90025c3907b01abc4fac081092921c
JakubKocerha <jakubbubble@gmail.com>
2022-01-01 21:39:03 +0000

    Create add_post template

--- static/js/script.js
+++ static/js/script.js
@@ -7,4 +7,8 @@ $(document).ready(function(){
             outDuration: 400
         });
     $('.materialboxed').materialbox();
+    $('#post_body').val('');
+    M.textareaAutoResize($('#post_body'));
+    $('input#title_text, input#title_text, textarea#summary, textarea#post_body').characterCounter();
+
 });
