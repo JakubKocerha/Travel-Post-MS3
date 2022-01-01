e2c4926ad6572fb5471e712c39bf15ab3ca8aa48
JakubKocerha <jakubbubble@gmail.com>
2022-01-01 18:00:46 +0000

    Add Post collapsible setup

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.modal').modal();
+    $('.collapsible').collapsible(
+        {
+            inDuration: 400,
+            outDuration: 400
+        });
+    $('.materialboxed').materialbox();
 });
