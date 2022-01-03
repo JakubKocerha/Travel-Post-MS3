beb06d849f9236311f945d437ed890fa28eafb19
JakubKocerha <jakubbubble@gmail.com>
2022-01-03 06:44:34 +0000

    Add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -10,5 +10,11 @@ $(document).ready(function(){
     $('#post_body').val('');
     M.textareaAutoResize($('#post_body'));
     $('input#title_text, input#title_text, textarea#summary, textarea#post_body').characterCounter();
-
+    $('.datepicker').datepicker({
+        format: "dd mmmm, yyyy",
+        showClearBtn: true,
+        i18n:{
+            done: "Select"
+        }
+    });
 });
